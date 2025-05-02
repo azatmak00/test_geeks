c6f052bcb67be3a317b40ec38c2e387b0f0c557b
Макетаев Азат Болотбекович <azatmaketaev01@MacBook-Pro-Maketaev.local>
2025-05-02 15:18:51 +0600

    first commit

---
+++ pokemon-list/src/App.jsx
@@ -0,0 +1,53 @@
+// src/App.jsx
+import React, { useEffect, useState } from "react";
+import PokemonCard from "./components/PokemonCard";
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const data = await res.json();
+      const pokemonData = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+          };
+        })
+      );
+      setPokemons(pokemonData);
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#fbeecb",
+        minHeight: "100vh",
+        padding: "40px",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "20px",
+        }}
+      >
+        {pokemons.map((p) => (
+          <PokemonCard key={p.name} name={p.name} image={p.image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemon-list/src/components/PokemonCard.jsx
@@ -0,0 +1,25 @@
+// src/components/PokemonCard.jsx
+import React from "react";
+
+const PokemonCard = ({ name, image }) => (
+  <div
+    style={{
+      backgroundColor: "#addcf6",
+      borderRadius: "12px",
+      padding: "15px",
+      width: "200px",
+      height: "100px",
+      display: "flex",
+      alignItems: "center",
+      gap: "10px",
+      boxShadow: "3px 3px 10px rgba(0, 0, 0, 0.2)",
+    }}
+  >
+    <img src={image} alt={name} style={{ width: "50px", height: "50px" }} />
+    <h3 style={{ margin: 0, textTransform: "capitalize", color: "#182b3a" }}>
+      {name}
+    </h3>
+  </div>
+);
+
+export default PokemonCard;
